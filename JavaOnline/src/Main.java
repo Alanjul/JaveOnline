17f4bf59aa3dcabd21442f4ad79ddc4bbda4e241
Julius Niyonzima <Juliusniyonzima@gmail.com>
2022-07-02 23:53:36 -0500

    home work

--- JavaOnline/src/Main.java
+++ JavaOnline/src/Main.java
@@ -1,6 +1,15 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello Java!");
+
+        System.out.println("hello Java");
+        System.out.println("This is my first sentence");
+        System.out.println("I am new to Java");
+        System.out.println("This JAva course is cool");
+        System.out.println("I am learning stuff every day");
+
+
     }
+
+
 
 }
