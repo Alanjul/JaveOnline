6908a176348b51969bca1f04b6c3a9f12ce123aa
Alanjul <Juliusniyonzima423@gmail.com>
2022-08-04 07:29:11 -0500

    lesson5 primitive data type and more

--- JavaOnline/src/Main.java
+++ JavaOnline/src/Main.java
@@ -1,22 +1,36 @@
+import com.fundamentals.lessons.Lesson5.Lesson5;
+
 public class Main {
     public static void main(String[] args) {
     // write your code here
 
         /**
-         * this main class is the entry point to the  applicatiob
+         * this main class is the entry point to the  application
          **/
-        System.out.println("hello Java");
-        System.out.println("This is my first sentence");
-        System.out.println("I am new to Java");
-        System.out.println("This JAva course is cool");
-        System.out.println("I am learning stuff every day");
+        //System.out.println("welcome Java");
+        //System.out.println("This is my first sentence");
+       // System.out.println("I am new to Java");
+       // System.out.println("This JAva course is cool");
+        //System.out.println("I am learning stuff every day");
 
-        House myHouse = new House();
-
-
-
-    }
+        //House myHouse = new House();
+        lesson5Examples();
 
 
 
-}
+    } //end main method
+
+    public static void lesson5Examples() {
+        Lesson5 myLesson5 = new Lesson5();
+        myLesson5.showLocalExample();
+        myLesson5.showLocalWithParameter(10,15);
+        int value = myLesson5.showLocalWithReturn(25,19);
+        System.out.println(value);
+        System.out.println(myLesson5.showLocalWithReturn(5,8));
+        myLesson5.showInstanceVariable();
+        myLesson5.showConversions();
+    } // end lesson5Example method
+
+
+
+} //end class
---
+++ JavaOnline/src/sameMethod.java
@@ -0,0 +1,15 @@
+public class sameMethod {
+    public static void main(String[] args) {
+        new sameMethod();
+    }
+    // statements below from lesson 3
+    static void sameMethod() {
+        System.out.println("welcome to Java");
+        System.out.println("This is my first sentence");
+        System.out.println("Iam new to Java");
+        System.out.println("This java course is very cool");
+        System.out.println("I learn new stuff every day");
+    }
+
+
+}
