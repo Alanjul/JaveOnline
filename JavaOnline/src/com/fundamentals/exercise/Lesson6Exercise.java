33e37d6f2685c89e3f03ca273c23154b33447e84
Alanjul <Juliusniyonzima423@gmail.com>
2022-08-16 10:02:16 -0500

    exercise

---
+++ JavaOnline/src/com/fundamentals/exercise/Lesson6Exercise.java
@@ -0,0 +1,26 @@
+package com.fundamentals.exercise;
+
+public class Lesson6Exercise {
+    String myString = "goodmorning";
+
+    public void exerciseCharAt() {
+        char myChar =myString.charAt(10);
+        System.out.println(myChar);
+    }// end method ExerciseCharAt
+
+    public String exerciseLowerCase() {
+
+        return myString.toLowerCase();
+    }//end exerciseLowerCase
+    char pilcrow ='\u00b6';
+    public void exerciseChar() {
+        System.out.println(pilcrow);
+    }// end method exerciseChar
+    public String exerciseUpperCase() {
+        return myString.toUpperCase();
+    }//end exercise upper class
+
+    public void exerciseEscape() {
+        System.out.println("Jack and Jill \nWent up the hill \n To fetch a pail of water. \n Jack fell down and broke his crown \n And Jill came tambling after.");
+    }//end of method escape
+}
