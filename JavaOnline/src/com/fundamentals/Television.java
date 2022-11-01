babbad3ed266534a74b8eff1285677dc7c3aa05f
Alanjul <Juliusniyonzima423@gmail.com>
2022-10-31 20:44:35 -0500

    Television Extended

---
+++ JavaOnline/src/com/fundamentals/Television.java
@@ -0,0 +1,24 @@
+package com.fundamentals;
+
+import com.fundamentals.exercise.Lesson11Exercise;
+
+/*
+* This class is a sub class of television*/
+public class Television extends Lesson11Exercise {
+    private int  screenSize;
+    public Television() {
+        System.out.println("Default televison constructor");
+    }   public Television(int  screenSize){
+            this. screenSize();
+        }
+
+    public int   screenSize() {
+        return  screenSize();
+    }
+    public void setMountable(boolean isMountable){
+        this. screenSize = screenSize;
+    }
+    public void Inches(){
+        System.out.println("will buy a small inch television set");
+    }
+}
